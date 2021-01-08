71adfac6bc59d3d3ad052db19716fbfe7890d320
praise-laurine <praiselaurine10@gmail.com>
2021-01-08 14:07:15 +0300

    deployment

--- tribune/settings.py
+++ tribune/settings.py
@@ -82,14 +82,14 @@ WSGI_APPLICATION = 'tribune.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 if config('MODE')=="dev":
-DATABASES = {
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
-           'PORT': '',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '',
     }
 }
 # production
